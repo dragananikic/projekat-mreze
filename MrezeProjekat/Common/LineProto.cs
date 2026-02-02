046caee30cc86f5cbe4980e9daf849b423478166
danijelasavic1802 <danijelasavv18@gmail.com>
2026-02-02 19:31:52 +0100

    Pocetna struktura projekta

---
+++ MrezeProjekat/Common/LineProto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class LineProto
+    {
+        public static void SendLine(Socket s, string line)
+        {
+            var data = Encoding.UTF8.GetBytes(line + "\n");
+            s.Send(data);
+        }
+
+        public static IEnumerable<string> ExtractLines(StringBuilder sb)
+        {
+            while (true)
+            {
+                var str = sb.ToString();
+                int idx = str.IndexOf('\n');
+                if (idx < 0) yield break;
+
+                string line = str.Substring(0, idx).TrimEnd('\r');
+                sb.Remove(0, idx + 1);
+                yield return line;
+            }
+        }
+    }
+}
---
+++ MrezeProjekat/Common/ZadatakProjekta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public enum StatusZadatka
+    {
+        NaCekanju,
+        UToku,
+        Zavrsen
+    }
+
+    public class ZadatakProjekta
+    {
+        public string Naziv { get; set; } = "";
+        public string Zaposleni { get; set; } = "";   
+        public StatusZadatka Status { get; set; } = StatusZadatka.NaCekanju;
+        public DateTime Rok { get; set; }
+        public int Prioritet { get; set; }            
+        public string? Komentar { get; set; }        
+        public string Menadzer { get; set; } = "";
+    }
+}
